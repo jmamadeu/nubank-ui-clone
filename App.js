bab01487063102ca3f6e6055a61784286da361e3
João Amadeu <jmamadeu2000@gmail.com>
2020-04-28 13:37:40 +0100

    Configurando a status bar

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
+
+import Main from './src/pages/Main';
 
 export default function App() {
+  const hexaColor = '#8B10AE';
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <>
+      <View style={{ flex: 1, backgroundColor: hexaColor }}>
+        <StatusBar barStyle='light-content' backgroundColor={hexaColor} />
+        <Main />
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
